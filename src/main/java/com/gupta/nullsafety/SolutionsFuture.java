bb1d231c729365797e0613921b47f5c908f127c8
Mala Gupta <mala.gupta@gmail.com>
2026-03-17 17:57:37 +0530

    Valhalla

--- src/main/java/com/gupta/nullsafety/SolutionsFuture.java
+++ src/main/java/com/gupta/nullsafety/SolutionsFuture.java
@@ -1,4 +1,8 @@
 package com.gupta.nullsafety;
 
 public class SolutionsFuture {
+    // Valhalla
+
+    //record Coffee(String! name, String! origin, String! brewingInstructions) {}
+    //record Order(String! customerName, Coffee! coffee) {}
 }
